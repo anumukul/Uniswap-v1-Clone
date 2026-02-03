4a72e0ada3bd2a051e5408bffceaae6965bcf1af
Anubhav Singh <anubhavsingh@Mac.lan>
2026-02-04 02:09:20 +0530

    fixed the vercel build errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
       net: false,
       tls: false,
     };
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        '@react-native-async-storage/async-storage': false,
+        'pino-pretty': false,
+      };
+    }
+
+    config.ignoreWarnings = [
+      { module: /node_modules\/@metamask\/sdk/ },
+      { module: /node_modules\/pino/ },
+    ];
+
     return config;
   },
 }
